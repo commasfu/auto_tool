1e2ebd97401dc83d90bf33654cc1cac43790c06e
fcx <fchengxiang@163.com>
2018-11-01 11:38:10 +0800

    public package use xcl

--- test/test.py
+++ test/test.py
@@ -3,6 +3,14 @@
 # author:commas
 # datetime:2018/10/31 11:31
 
+import os
 import time
 
+from conf.settings import *
+
 print(time.strftime('%Y-%m-%d %H:%M:%S'))
+
+
+print(RPROJECT_ROOT_PATH)
+
+print(os.path.dirname(__file__))
