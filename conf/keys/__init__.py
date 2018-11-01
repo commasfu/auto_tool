d84274ae526295f252361cb2d3322d6410f66f07
fcx <fchengxiang@163.com>
2018-11-01 10:29:26 +0800

    public package

---
+++ conf/keys/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Author : xchliu
+    Date   : 06/06/2017
+    Desc   : 
+    Change LOG : 
+    
+"""
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
--- conf/setting.py
+++ conf/setting.py
@@ -10,3 +10,14 @@ import os
 PATH_ROOT = '/data'
 ROOT_LOG = os.path.join(PATH_ROOT, 'logs', 'auto_tool.log')
 DEBUG = False
+
+RPROJECT_ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
+
+# socket
+MANAGEMENT_HOST = ('192.168.200.89', 5522)
+SOCKET_DEFAULT_TIMEOUT = 10
+SSL_KEYS = dict(
+                keyfile=os.path.join(RPROJECT_ROOT_PATH,'conf','keys', "client-key.pem"),
+                certfile=os.path.join(RPROJECT_ROOT_PATH,'conf','keys', "client-cert.pem"),
+                ca_certs=os.path.join(RPROJECT_ROOT_PATH,'conf','keys', "ca-cert.pem")
+                )
