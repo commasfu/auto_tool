8e37edcbc8042531cef1880618fa34e1df8eab4f
fcx <fchengxiang@163.com>
2018-11-12 15:44:24 +0800

    by commas

--- test/mysite/auto_web/views.py
+++ test/mysite/auto_web/views.py
@@ -16,13 +16,29 @@ def index(request):
     if request.method == 'POST':
         user = request.POST.get("user", None)
         password = request.POST.get("password", None)
-        print(user, password)
-        models.UserInfo.objects.create(user=user, password=password)
+        # models.UserInfo.objects.create(user=user, password=password)
+        db_user_pwd = list(models.UserInfo.objects.all().values_list('user', 'password'))
+        for param in db_user_pwd:
+            if (user, password) == param:
+                return render(request, 't_html.html')
         # temp = {'username': username, 'password': password}
         # user_list.append(temp)
     user_list = models.UserInfo.objects.all()
     return render(request, 'index.html',{"data": user_list})
 
 def login(request):
+    '''
 
+    :param request:
+    :return:
+    '''
+    if request.method == 'POST':
+        user = request.POST.get("user", None)
+        password = request.POST.get("password", None)
+        print(user, password)
+        db_user_pwd = list(models.UserInfo.objects.all().values_list('user', 'password'))
+        for param in db_user_pwd:
+            if (user, password) == param:
+                return render(request, 't_html.html')
+        models.UserInfo.objects.create(user=user, password=password)
     return render(request, 'login.html')
